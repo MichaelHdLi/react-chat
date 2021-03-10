76d551da438a124ce0b24bef394b66d22e91b0f3
Michael LI <hl5@ualberta.ca>
2021-03-09 23:29:23 -0700

    fix

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-	document.getElementById("root")
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App />
+    </BrowserRouter>,
+    document.getElementById("root")
 );
